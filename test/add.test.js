79e8019376b60c5e546b3b7a6c57c7dd70477101
wangding <408542507@qq.com>
2020-06-18 21:11:14 +0800

    dynamic test

--- test/add.test.js
+++ test/add.test.js
@@ -14,3 +14,33 @@ describe('加法函数的单元测试', function(){   // 测试套件
     expect(add('a', 'b')).to.be.NaN;
   });
 });
+
+describe('加法函数的动态测试', function() {
+  var tests_normal = [
+    {args: [0,          0], exp: 0},
+    {args: [2,          3], exp: 5},
+    {args: [-4,        -2], exp: -6},
+    {args: [-4,        10], exp: 6},
+    {args: [1,       1023], exp: 1024},
+    {args: [2.6,      3.8], exp: 6.4},
+    {args: ['2e2', '3e-2'], exp: 200.03}
+  ];
+
+  tests_normal.forEach(function(test) {
+    it(`${test.args[0]} + ${test.args[1]} = ${test.exp}`, function() {
+      expect(add(test.args[0], test.args[1])).to.be.equal(test.exp);
+    });
+  });
+
+  var tests_ugily = [
+    {args: ['a',      'b'], exp: NaN},
+    {args: [2,        'a'], exp: NaN},
+    {args: [',',        3], exp: NaN}
+  ];
+
+  tests_ugily.forEach(function(test) {
+    it(`${test.args[0]} + ${test.args[1]} = ${test.exp}`, function() {
+      expect(add(test.args[0], test.args[1])).to.be.NaN;
+    });
+  });
+});
